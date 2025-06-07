006a0aa31104246c0def7a8a66eecd211640c5ae
Vansh Rathod <132828677+vanshh13@users.noreply.github.com>
2025-06-07 22:18:50 +0530

    authentication added

--- src/main/java/com/HealthCare/AppointmentSystem/AppointmentSystemApplication.java
+++ src/main/java/com/HealthCare/AppointmentSystem/AppointmentSystemApplication.java
@@ -2,6 +2,7 @@ package com.HealthCare.AppointmentSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AppointmentSystemApplication {
@@ -9,5 +10,4 @@ public class AppointmentSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentSystemApplication.class, args);
 	}
-
 }
